ba608a84d71eae662794fd646e37c369ca11e97e
Andrew <andy.birks@gmail.com>
2015-04-08 21:52:47 +0100

    Player Detail Page

--- Controllers/FootballController.cs
+++ Controllers/FootballController.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+
+//Andrew adding this to get access to Models
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class FootballController : Controller
     {
-        // GET: Football
+        // GET: Index of Football
         public ActionResult Index()
         {
+            return View();
+        }
+
+
+        // GET: Football/Details/MUFC ???
+        public ActionResult Details(string id, string name)
+        {
+
+            if (id == null || name==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ViewBag.TeamId = id;
+            ViewBag.TeamName = name;
+
             return View();
         }
     }
